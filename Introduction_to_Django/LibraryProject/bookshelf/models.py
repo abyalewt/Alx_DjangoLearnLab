beea99c5cb0a0d1a794fa116b264dd0ccb0daa7c
abyalewtetsafe@gmail.com <abyalewtetsafe@gmail.com>
2026-01-10 23:28:00 +0300

    strated the CRUD operation

---
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+# This is the "Book" blueprint the inspector is looking for
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
