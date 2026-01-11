016f6c54349962a9f8325cddfd269ba19daa356e
abyalewtetsafe@gmail.com <abyalewtetsafe@gmail.com>
2026-01-11 11:23:11 +0300

    update the admin interface

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "author",
+        "publication_year",
+    )  # show these fields in list view
+    list_filter = ("publication_year", "author")  # add filters on the right side
+    search_fields = ("title", "author")  # enable search by title/author
