55d1bfb8bb346f5cd78dd917c8d6f9aa38585ff9
abyalewtetsafe@gmail.com <abyalewtetsafe@gmail.com>
2026-01-20 10:19:54 +0300

    update for the templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
- # ✅ checker requires this exact line
-from .models import Book
-from .models import Library  # ✅ checker requires this exact line
 from django.views.generic.detail import DetailView
+from .models import Book
+from .models import Library
 
 
-# Function-based view to list all books
 def list_books(request):
-    books = Book.objects.all()  # ✅ checker requires this
+    books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
 
-# Class-based view to display library details
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
