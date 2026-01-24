c92c35ab2df7d04330f9937350b6d7826421a5f4
abyalewtetsafe@gmail.com <abyalewtetsafe@gmail.com>
2026-01-24 10:17:54 +0300

    add permission requirment

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.contrib.auth import authenticate, logout
 from django.contrib.auth import login  # ✅ checker requires this exact line
+from django.contrib.auth.decorators import permission_required
 
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import user_passes_test
