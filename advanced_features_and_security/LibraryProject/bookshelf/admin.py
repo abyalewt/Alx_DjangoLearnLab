71832810e4c7e1ab25f851ab4b2967d2926d97f1
abyalewtetsafe@gmail.com <abyalewtetsafe@gmail.com>
2026-01-25 22:00:20 +0300

    update the admin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import Book, CustomUser
 
 
 @admin.register(Book)
@@ -9,4 +10,16 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ("title", "author")
 
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ["username", "email", "date_of_birth", "is_staff", "is_superuser"]
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+
+# ✅ Required by the checker
+admin.site.register(CustomUser, CustomUserAdmin)
